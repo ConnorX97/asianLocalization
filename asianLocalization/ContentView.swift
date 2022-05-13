b9488cebe5dcc9b5f0cf33e72c4f3962d7e08958
we <wer>
2022-05-13 19:49:13 +0900

    Initial Commit

---
+++ asianLocalization/ContentView.swift
@@ -0,0 +1,54 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var welcome = "str_welcome".localized()
+    
+    var body: some View {
+        VStack{
+            VStack{
+                Text(welcome)
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack(spacing:0){
+                VStack{
+                    Button(action: {
+                        Bundle.setLanguage(lang: "en")
+                        welcome = "str_welcome".localized()
+                    },label:{
+                        Text("English").foregroundColor(Color.white)
+                            .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.red)
+                    })
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                VStack{
+                    Button(action: {
+                        Bundle.setLanguage(lang: "zh-HK")
+                        welcome = "str_welcome".localized()
+                    },label:{
+                        Text("Chinese").foregroundColor(Color.white)
+                            .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.blue)
+                    })
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                VStack{
+                    Button(action: {
+                        Bundle.setLanguage(lang: "ko")
+                        welcome = "str_welcome".localized()
+                    },label:{
+                        Text("Korean").foregroundColor(Color.white)
+                            .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.green)
+                    })
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            }.frame(maxWidth: .infinity, maxHeight: 70)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.locale, .init(identifier: "en"))
+    }
+}
---
+++ asianLocalization/asianLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  asianLocalizationApp.swift
+//  asianLocalization
+//
+//  Created by Sherzod Fayziev on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct asianLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
